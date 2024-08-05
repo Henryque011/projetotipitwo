e221c0de82db6ab9fb03b1c6e59ef8defabab2e4
Henryque Nonato Silva <132319412+Henryque011@users.noreply.github.com>
2024-08-05 17:28:35 -0300

    Update acessibilidade.php

--- projetotipitwo/conteudo/acessibilidade.php
+++ projetotipitwo/conteudo/acessibilidade.php
@@ -1,11 +1,8 @@
 <article class="centro">
-            <nav>
-                <div class="acessibilidade">
-                    <button i class="fa-solid fa-eye-low-vision"></i></button>
-                    <button i class="fa-solid fa-hands"></i> </button>
-                </div>
-            </nav>
-            <div class="introsobre">
-                <h2> &lt; UM POUCO SOBRE &gt; </h2>
+        <nav>
+            <div class="acessibilidade">
+                <button i class="fa-solid fa-eye-low-vision"></i> </button>
+                <button i class="fa-solid fa-hands"></i> </button>
             </div>
-        </article>
+        </nav>
+</article>
