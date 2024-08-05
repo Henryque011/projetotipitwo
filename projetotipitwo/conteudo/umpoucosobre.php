46f4f9ba994ec43f4bb19dd533987c0750b1964f
Henryque Nonato Silva <132319412+Henryque011@users.noreply.github.com>
2024-08-05 17:30:47 -0300

    Update umpoucosobre.php

--- projetotipitwo/conteudo/umpoucosobre.php
+++ projetotipitwo/conteudo/umpoucosobre.php
@@ -1,27 +1,27 @@
 <article class="conteudo">
-            <div class="caixa">
-                <div class="imgilustrativa">
-                    <img src="img/imagem (1).png" alt="Icone">
+        <div class="caixa">
+            <div class="imgilustrativa">
+                <img src="img/imagem (1).png" alt="Icone">
+            </div>
+            <div class="caixatxt">
+                <p>
+                    Na TIPITWO, acreditamos que a web deve ser um lugar acessível para todos. Nossa missão é criar
+                    websites inclusivos, garantindo que todas as pessoas, independentemente de suas habilidades ou
+                    limitações, possam navegar e interagir com o conteúdo digital de maneira eficaz.</p>
+            </div>
+            <div>
+                <div class="caixaimg">
+                    <img src="img/estrtura de codigos.png" alt="Estrutura de códigos">
+                    <img src="img/toque de mãos em grupo.jpg" alt="Toque de mãos em grupo">
                 </div>
-                <div class="caixatxt">
-                    <p>
-                        Na TIPITWO, acreditamos que a web deve ser um lugar acessível para todos. Nossa missão é criar
-                        websites inclusivos, garantindo que todas as pessoas, independentemente de suas habilidades ou
-                        limitações, possam navegar e interagir com o conteúdo digital de maneira eficaz.</p>
-                </div>
-                <div>
-                    <div class="caixaimg">
-                        <img src="img/estrtura de codigos.png" alt="Estrutura de códigos">
-                        <img src="img/toque de mãos em grupo.jpg" alt="Toque de mãos em grupo">
-                    </div>
-                    <div class="caixaimg">
-                        <img src="img/gráfico no laptop.png" alt="Gráfico no laptop">
-                        <img src="img/comunicação libras.jpg" alt="Comunicação em libras">
-                    </div>
-                </div>
-                <div class="direcionamento">
-                    <!-- <button> <a href="#">PORTFÓLIO </a></button> link portifolio inativo ainda -->
-                    <button> <a href="sobre.php">SAIBA MAIS </a></button>
+                <div class="caixaimg">
+                    <img src="img/gráfico no laptop.png" alt="Gráfico no laptop">
+                    <img src="img/comunicação libras.jpg" alt="Comunicação em libras">
                 </div>
             </div>
+            <div class="direcionamento">
+                <!-- <button> <a href="#">PORTFÓLIO </a></button> link portifolio inativo ainda -->
+                <button> <a href="sobre.php">SAIBA MAIS </a></button>
+            </div>
+        </div>
 </article>
