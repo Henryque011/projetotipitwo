1e5bee404591c4314a98684a99a3e1e8e0b6ec9c
Henryque Nonato Silva <132319412+Henryque011@users.noreply.github.com>
2024-08-05 17:28:57 -0300

    Update banner1.php

--- projetotipitwo/conteudo/banner1.php
+++ projetotipitwo/conteudo/banner1.php
@@ -1,5 +1,4 @@
 <div class="banner1">
-    
     <div class="txtbanner">
             <div class="animate__animated animate__slideInLeft">
                 <h2>DESENVOLVIMENTO</h2>
@@ -8,7 +7,6 @@
                     <h3 class="dif">ACESSÍVEL</h3>
                 </div>
             </div>
-            
             <div class="animate__animated animate__zoomIn">
                 <h2>TRANSFORMANDO</h2>
                 <div class="posit">
@@ -17,6 +15,5 @@
                 </div>
                 <h2 class="pos2">CÓDIGOS</h2>
             </div>  
-           
     </div>
 </div>
