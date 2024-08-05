df070743151775515d93caa737c41a852bc3443b
Henryque Nonato Silva <132319412+Henryque011@users.noreply.github.com>
2024-08-05 17:29:18 -0300

    Update perguntas.php

--- projetotipitwo/conteudo/perguntas.php
+++ projetotipitwo/conteudo/perguntas.php
@@ -1,29 +1,29 @@
 <section class="perguntas">
-            <div class="faq">
-                <h2>Responsabilidade Social</h2>
-            </div>
-            <div class="res">
-                <h2>
-                    Impacto Positivo: Contribuir para uma internet mais acessível ajuda a criar uma sociedade mais
-                    inclusiva e justa.
-                </h2>
-            </div>
-            <div class="faq">
-                <h2>Inclusão e Igualdade</h2>
-            </div>
-            <div class="res">
-                <h2>
-                    Igualdade de Oportunidades: Promover a inclusão digital garante que ninguém seja excluído do acesso
-                    a informações, serviços e oportunidades online.
-                </h2>
-            </div>
-            <div class="faq">
-                <h2>Alcance Ampliado</h2>
-            </div>
-            <div class="res">
-                <h2>
-                    Um site acessível pode ser usado por um público maior, incluindo pessoas com deficiência, idosos e
-                    usuários com conexões de internet mais lentas.
-                </h2>
-            </div>
+        <div class="faq">
+            <h2>Responsabilidade Social</h2>
+        </div>
+        <div class="res">
+            <h2>
+                Impacto Positivo: Contribuir para uma internet mais acessível ajuda a criar uma sociedade mais
+                inclusiva e justa.
+            </h2>
+        </div>
+        <div class="faq">
+            <h2>Inclusão e Igualdade</h2>
+        </div>
+        <div class="res">
+            <h2>
+                Igualdade de Oportunidades: Promover a inclusão digital garante que ninguém seja excluído do acesso
+                a informações, serviços e oportunidades online.
+            </h2>
+        </div>
+        <div class="faq">
+            <h2>Alcance Ampliado</h2>
+        </div>
+        <div class="res">
+            <h2>
+                Um site acessível pode ser usado por um público maior, incluindo pessoas com deficiência, idosos e
+                usuários com conexões de internet mais lentas.
+            </h2>
+        </div>
 </section>
